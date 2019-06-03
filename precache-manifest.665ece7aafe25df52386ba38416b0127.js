45cae43b2527cf422ccd8503e434ccd6b6578d88
slavikator <slavikator@gmail.com>
2019-06-03 22:42:16 +0300

    Updates

---
+++ precache-manifest.665ece7aafe25df52386ba38416b0127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cae043abde1f5938a946af1a601dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02cea1e97efdf03c8373",
+    "url": "/static/css/main.757e5449.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "02cea1e97efdf03c8373",
+    "url": "/static/js/main.d865e104.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
